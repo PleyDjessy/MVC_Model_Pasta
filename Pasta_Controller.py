2a7c5f1ba7b3f9af6afa35e51fe4962651a35831
PleyDjessy <pleydjessy.09@gmail.com>
2024-10-10 00:50:50 +0200

    Добавлен файл Pasta_Controller, в нем создан и завершен класс PastaController. Файл main обновлён.

---
+++ Pasta_Controller.py
@@ -0,0 +1,26 @@
+class PastaController:
+    def __init__(self,model):
+        self.__model = model
+
+    def get_data(self):
+        return f"Название: {self.__model.get_name()}, ингредиенты: {self.__model.get_ingredients()}, цена: {self.__model.get_price()}, вес: {self.__model.get_weight()}"
+
+    def change_data(self, user_rights, type_of_data, data):
+        if user_rights == "admin":
+            self.__model.change_data(type_of_data, data)
+            return "Успешно"
+        else:
+            return "Ошибка: нет доступа"
+
+    def append_custom_ingredients(self, user_rights):
+        if user_rights == "admin":
+            self.__model.add_custom_ingredients()
+        else:
+            return "Ошибка: нет доступа"
+
+    def client_own_pasta(self, user_rights, client_ingredients: list, client_weight: float):
+        if user_rights in ("admin", "customer"):
+            return self.__model.client_own_pasta(client_ingredients, client_weight)
+        else:
+            return "Ошибка: нет доступа"
+
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from Pasta_Model import PastaModel
+from Pasta_Controller import PastaController
 
-spaghetti = PastaModel("spaghetti", ["makarons", "tomatoes", "chicken", "souse", "green"], 200.0, 1.0, ["more_green!!!"])
+spaghetti = PastaModel("spaghetti", ["pasta", "tomatoes", "chicken", "souse", "green"], 200.0, 1.0, ["more_green!!!"])
+c_spaghetti = PastaController(spaghetti)
 
 print(spaghetti.get_ingredients())
 
 spaghetti.add_custom_ingredients()
 print(spaghetti.get_ingredients())
 
-print(spaghetti.client_own_pasta(["makarons","MANY MANY GREEEEEEEN!!!"], 5.0))
+print(spaghetti.client_own_pasta(["pasta","MANY MANY GREEEEEEEN!!!"], 5.0), "\n")
+
+
+print(c_spaghetti.get_data())
+print(c_spaghetti.change_data("user", "name", "SEfsfwewfsffF"))
+c_spaghetti.change_data("admin", "weight", 1.4)
+print(c_spaghetti.get_data())
+
+print(c_spaghetti.client_own_pasta("customer", ["entire chicken filled with pasta inside", "some green outside"], 7.999))
